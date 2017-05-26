85aacfe6498bdd0613b841673a34974cd5befe99
이남규 <nklee@ncsoft.com>
2017-05-26 11:05:05 +0900

    list docker container name of pod 명령어 추가

--- kubectl-command.sh
+++ kubectl-command.sh
@@ -12,19 +12,21 @@ help()
     echo "First Argument: action"
     echo "Second Argument: application name"
     echo "Third Argument: docker container name"
-    echo ""
+    echo
     echo "[Action Info]"
-    echo "1 -> kubectl describe : show detailed information about a resource"
-    echo "2 -> kubectl logs : print the logs from a container in a pod"
-    echo "3 -> kubectl get : list resources"
-    echo "4 -> kubectl exec : execute a command on a container in a pod"
-    echo ""
+    echo "1 -> show detailed information about a resource"
+    echo "2 -> print the logs from a container in a pod"
+    echo "3 -> list resources"
+    echo "4 -> execute a command on a container in a pod"
+    echo "5 -> list docker container name of pod"
+    echo
     echo "[Example]"
     echo "1 -> ./kubectl-command.sh 1 bootcamp"
     echo "2 -> ./kubectl-command.sh 2 bootcamp"
     echo "3 -> ./kubectl-command.sh 3"
     echo "4 -> ./kubectl-command.sh 4 bootcamp kubernetes-bootcamp"
-    echo "------------------------------------------------------------------------------------"
+    echo "5 -> ./kubectl-command.sh 5 bootcamp"
+    echo "------------------------------------------------------------------------------------ \n\n"
 }
 
 help
@@ -47,8 +49,13 @@ case $ACTION in
 4)
   kubectl exec -ti $POD_NAME -c $DOCKER_CONTAINER_NAME /bin/bash
   ;;
+5)
+  kubectl get pods $POD_NAME -o=custom-columns=NAME:.metadata.name,CONTAINERS:.spec.containers[*].name
+  ;;
 *)
   echo "[FAIL] invalid action parameter!!" && exit 1
 esac
 
+echo
+echo
 echo "Done!!"
