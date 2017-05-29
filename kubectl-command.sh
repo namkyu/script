77439d7dc2bee2d0e8348635e76d6403c766fd8a
이남규 <nklee@ncsoft.com>
2017-05-29 16:18:50 +0900

    kubectl 명령어 수정

--- kubectl-command.sh
+++ kubectl-command.sh
@@ -9,9 +9,9 @@ help()
     echo "	                                M A I N - M E N U"
     echo "------------------------------------------------------------------------------------"
     echo "[Argument Info]"
-    echo "First Argument: action"
-    echo "Second Argument: application name"
-    echo "Third Argument: docker container name"
+    echo "First Argument: ACTION"
+    echo "Second Argument: APPLICATION_NAME"
+    echo "Third Argument: DOCKER_CONTAINER_NAME"
     echo
     echo "[Action Info]"
     echo "1 -> show detailed information about a resource"
@@ -21,11 +21,11 @@ help()
     echo "5 -> list docker container name of pod"
     echo
     echo "[Example]"
-    echo "1 -> ./kubectl-command.sh 1 bootcamp"
-    echo "2 -> ./kubectl-command.sh 2 bootcamp"
-    echo "3 -> ./kubectl-command.sh 3"
-    echo "4 -> ./kubectl-command.sh 4 bootcamp kubernetes-bootcamp"
-    echo "5 -> ./kubectl-command.sh 5 bootcamp"
+    echo "1 -> ./kubectl-command.sh [ACTION] [APPLICATION_NAME]"
+    echo "2 -> ./kubectl-command.sh [ACTION] [APPLICATION_NAME] [DOCKER_CONTAINER_NAME]"
+    echo "3 -> ./kubectl-command.sh [ACTION]"
+    echo "4 -> ./kubectl-command.sh [ACTION] [APPLICATION_NAME] [DOCKER_CONTAINER_NAME]"
+    echo "5 -> ./kubectl-command.sh [ACTION] [APPLICATION_NAME]"
     echo "------------------------------------------------------------------------------------ \n\n"
 }
 
@@ -34,23 +34,28 @@ help
 ACTION=$1
 APP_NAME=$2
 DOCKER_CONTAINER_NAME=$3
-POD_NAME=$(kubectl get pods | awk -v pattern="$APP_NAME" '$0 ~ pattern {print $1}' | head -1)
+POD_NAME=$(kubectl get pods --all-namespaces | awk -v pattern="$APP_NAME" '$0 ~ pattern {print $2}' | head -1)
+POD_NAMESPACE=$(kubectl get pods --all-namespaces | awk -v pattern="$APP_NAME" '$0 ~ pattern {print $1}' | head -1)
+
+echo "----------------------------------------------------------"
+echo "POD_NAME : $POD_NAME, POD_NAME_SPACE : $POD_NAMESPACE"
+echo "----------------------------------------------------------"
 
 case $ACTION in
 1)
-  kubectl describe pods $POD_NAME
+  kubectl describe pods $POD_NAME --namespace=$POD_NAMESPACE
   ;;
 2)
-  kubectl logs $POD_NAME
+  kubectl logs $POD_NAME --namespace=$POD_NAMESPACE -c $DOCKER_CONTAINER_NAME
   ;;
 3)
-  kubectl get pods
+  kubectl get pods --all-namespaces
   ;;
 4)
-  kubectl exec -ti $POD_NAME -c $DOCKER_CONTAINER_NAME /bin/bash
+  kubectl exec -ti $POD_NAME -c $DOCKER_CONTAINER_NAME /bin/bash --namespace=$POD_NAMESPACE
   ;;
 5)
-  kubectl get pods $POD_NAME -o=custom-columns=NAME:.metadata.name,CONTAINERS:.spec.containers[*].name
+  kubectl get pods $POD_NAME -o=custom-columns=NAME:.metadata.name,CONTAINERS:.spec.containers[*].name --namespace=$POD_NAMESPACE
   ;;
 *)
   echo "[FAIL] invalid action parameter!!" && exit 1
